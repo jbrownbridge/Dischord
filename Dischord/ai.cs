16ebf924f0b5258c6c3a4e6a33f927fe88073515
Marco <marco@gallotta.co.za>
2010-01-30 12:21:16 +0200

    Fix tiny bug and comment code

--- Dischord/ai.cs
+++ Dischord/ai.cs
@@ -33,19 +33,19 @@ namespace Dischord
             int[,] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
             Direction[] dir = { Direction.up, Direction.down, Direction.left, Direction.right };
             PriorityQueue<int, PQEntry> queue = new PriorityQueue<int,PQEntry>();
-            queue.Enqueue(0, new PQEntry(0, target, pos));
+            queue.Enqueue(0, new PQEntry(0, target, pos)); // going from target to pos, purposefully in reverse
             while (!queue.IsEmpty)
             {
                 PQEntry e = queue.Dequeue();
                 for (int i = 0; i < d.Length; i++)
                 {
-                    MapCell cell = map.getCell(e.pos.X + d[i,0], e.pos.Y + d[i,1]);
-                    if (cell.Type == MapCell.MapCellType.floor)
+                    MapCell cell = map.getCell(e.pos.X + d[i,0], e.pos.Y + d[i,1]); // cell we would move onto
+                    if (cell.Type == MapCell.MapCellType.floor) // can only go through floors
                     {
                         PQEntry newEntry = new PQEntry(e.cost + cell.cost(), new Point(e.pos.X + d[i, 0], e.pos.Y + d[i, 1]), pos);
                         queue.Enqueue(newEntry.heuristic, newEntry);
                         Point p = new Point(pos.X + d[i,0], pos.Y + d[i,1]);
-                        if (p == target)
+                        if (p == pos) // if we've reached our "target", then this must be the best path; we only care about one move for now, since the obstacles might change before reaching the target
                         {
                             return dir[i];
                         }
