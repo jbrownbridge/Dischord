4e6488b8bfffa345adf3caaa82d0baeb028be06a
Marco <marco@gallotta.co.za>
2010-01-30 12:29:53 +0200

    Don't revist cells in path-finding

--- Dischord/ai.cs
+++ Dischord/ai.cs
@@ -31,24 +31,27 @@ namespace Dischord
         public static Direction findPath(Map map, Point pos, Point target)
         {
             int[,] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            bool[,] vis = new bool[map.width, map.height];
             Direction[] dir = { Direction.up, Direction.down, Direction.left, Direction.right };
             PriorityQueue<int, PQEntry> queue = new PriorityQueue<int,PQEntry>();
             queue.Enqueue(0, new PQEntry(0, target, pos)); // going from target to pos, purposefully in reverse
+            vis[target.X, target.Y] = true;
             while (!queue.IsEmpty)
             {
                 PQEntry e = queue.Dequeue();
                 for (int i = 0; i < d.Length; i++)
                 {
                     MapCell cell = map.getCell(e.pos.X + d[i,0], e.pos.Y + d[i,1]); // cell we would move onto
-                    if (cell.Type == MapCell.MapCellType.floor) // can only go through floors
+                    Point p = new Point(pos.X + d[i, 0], pos.Y + d[i, 1]);
+                    if (cell.Type == MapCell.MapCellType.floor && !vis[p.X, p.Y]) // can only go through floors
                     {
                         PQEntry newEntry = new PQEntry(e.cost + cell.cost(), new Point(e.pos.X + d[i, 0], e.pos.Y + d[i, 1]), pos);
                         queue.Enqueue(newEntry.heuristic, newEntry);
-                        Point p = new Point(pos.X + d[i,0], pos.Y + d[i,1]);
                         if (p == pos) // if we've reached our "target", then this must be the best path; we only care about one move for now, since the obstacles might change before reaching the target
                         {
                             return dir[i];
                         }
+                        vis[p.X, p.Y] = true;
                     }
                 }
             }
